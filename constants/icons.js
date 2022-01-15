6129ab0decfe1a26d121a284589ebc9b92cf6a43
daniel <danielpalak11@wp.pl>
2022-01-15 14:03:17 +0000

    czwarty commit

---
+++ constants/icons.js
@@ -0,0 +1,77 @@
+export const back = require("../assets/icons/back.png");
+export const car = require("../assets/icons/car.png");
+export const cutlery = require("../assets/icons/cutlery.png");
+export const dessert = require("../assets/icons/dessert.png");
+export const drink = require("../assets/icons/drink.png");
+export const fire = require("../assets/icons/fire.png");
+export const fries = require("../assets/icons/fries.png");
+export const hamburger = require("../assets/icons/hamburger.png");
+export const like = require("../assets/icons/like.png");
+export const list = require("../assets/icons/list.png");
+export const location = require("../assets/icons/location.png");
+export const master_card = require("../assets/icons/mastercard.png");
+export const nearby = require("../assets/icons/nearby.png");
+export const wrap = require("../assets/icons/wrap.png");
+export const pin = require("../assets/icons/pin.png");
+export const sandwich = require("../assets/icons/sandwich.png");
+export const red_pin = require("../assets/icons/red-pin.png");
+export const chicken = require("../assets/icons/chicken.png");
+export const salad = require("../assets/icons/salad.png");
+export const search = require("../assets/icons/search.png");
+export const basket = require("../assets/icons/shopping-basket.png");
+export const star = require("../assets/icons/star.png");
+export const sets = require("../assets/icons/sets.png");
+export const user = require("../assets/icons/user.png");
+export const cross = require("../assets/icons/cross.png");
+export const home = require("../assets/icons/home.png")
+export const cart = require("../assets/icons/cart.png");
+export const my_wallet = require("../assets/icons/wallet.png");
+export const coupons = require("../assets/icons/coupon.png");
+export const help = require("../assets/icons/help.png");
+export const invite = require("../assets/icons/profile.png");
+export const correct = require("../assets/icons/correct.png");
+export const eye_open = require("../assets/icons/eye.png");
+export const eye_close = require("../assets/icons/eye_close.png");
+export const logout = require("../assets/icons/logout.png");
+export const email = require("../assets/icons/email.png");
+
+
+export default {
+    back,
+    car,
+    cutlery,
+    drink,
+    fire,
+    fries,
+    hamburger,
+    sandwich,
+    like,
+    list,
+    location,
+    master_card,
+    nearby,
+    wrap,
+    pin,
+    chicken,
+    red_pin,
+    sets,
+    salad,
+    search,
+    basket,
+    star,
+    dessert,
+    user,
+    cross,
+    home,
+    cart,
+    my_wallet,
+    coupons,
+    help,
+    invite,
+    correct,
+    eye_open,
+    eye_close,
+    logout,
+    email,
+
+}
---
+++ constants/maps.js
@@ -0,0 +1,3 @@
+const GOOGLE_API_KEY = "AIzaSyDwGCt0MCaTvjs_khxp8of5J3B7GzRa4JE"
+
+export default GOOGLE_API_KEY
